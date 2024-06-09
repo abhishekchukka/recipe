cf69d7b43170e908b6fe73a883f481fe09cdf400
abhichukka08 <159679865+abhishekchukka@users.noreply.github.com>
2024-06-09 22:03:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {
   signInWithEmailAndPassword,
 } = require("firebase/auth");
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyB0fRn6Y_aAXsDdjoiNGHcJmMawKv34BDs",
   authDomain: "assignment-4f1cb.firebaseapp.com",
@@ -18,19 +18,19 @@ const firebaseConfig = {
   appId: "1:319331197445:web:476ec63472eec47eaff26c",
 };
 
-// Initialize Firebase
+
 const firebaseApp = initializeApp(firebaseConfig);
 
-// Middleware to parse JSON bodies
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
-// Serve the HTML file
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Endpoint to create a new user
+
 app.post("/register", async (req, res) => {
   try {
     const { email, username, password } = req.body;
@@ -51,11 +51,11 @@ app.post("/register", async (req, res) => {
       });
   } catch (e) {
     console.error(e);
-    res.redirect("/register"); // Redirect to register page on error
+    res.redirect("/register");
   }
 });
 
-// Endpoint to login a user
+
 app.post("/log-in", async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -66,21 +66,21 @@ app.post("/log-in", async (req, res) => {
         // Signed in
         const user = userCredential.user;
         console.log(user);
-        res.sendFile(path.join(__dirname, "login-success.html")); // Redirect to login success page
+        res.sendFile(path.join(__dirname, "login-success.html")); 
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(error);
-        res.status(400).send(`Error: ${errorMessage}`); // Send an error response
+        res.status(400).send(`Error: ${errorMessage}`); 
       });
   } catch (e) {
     console.error(e);
-    res.redirect("/login"); // Redirect to login page on error
+    res.redirect("/login"); 
   }
 });
 
-// Endpoint to fetch recipes
+
 app.get("/recipe", async (req, res) => {
   const recipeName = req.query.name;
   const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeName}`;
@@ -96,7 +96,7 @@ app.get("/recipe", async (req, res) => {
   }
 });
 
-// Start the Express server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
